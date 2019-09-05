fcdc2d6205aaab80a1ff19b4f51ae06de4b2f4de
Crystal <yxcwuu@gmail.com>
2019-09-05 01:11:13 -0400

    add calculator operation logic

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 const value = document.querySelector('#value');
 const buttons = document.querySelectorAll('.calculator__buttons--key');
+let equation = '';
+
 
 buttons.forEach(button => {
+    value.textContent = '';
+
+    function evaluation(operation) {
+    if (equation !== '' && equation[equation.length -1] !== operation) {
+        equation += operation;
+            value.textContent += button.textContent;
+        }   
+    }
+
+    function operation() {
+        if (equation[equation.length - 1] === '0' && equation[equation.length - 2] === '/') {
+            value.textContent = 'Cannot divide by 0';
+        } else {
+            equation = `${Number.isInteger(eval(equation)) ? eval(equation) : eval(equation).toFixed(3)}`;
+            value.textContent = equation;
+        }
+    }
+
     button.addEventListener('click', (() => {
-        console.log(button.textContent);
+        if (button.id === 'AC') {
+            equation = '';
+            value.textContent = equation;
+        } else if (button.id === 'reverse') {
+            equation = equation.slice(0, -1)
+            value.textContent = equation;
+        } else if (button.id === 'sign') {
+            equation *= -1;
+            value.textContent = equation;
+        } else if (button.id === 'multiply') {
+            evaluation('*');  
+        } else if (button.id === 'divide') {
+            evaluation('/');
+        } else if (button.textContent === '=' && equation !== '') {
+            operation();
+        } else {
+            value.textContent += button.textContent;
+            equation += button.textContent;
+
+        }
+        
     }));
 });
